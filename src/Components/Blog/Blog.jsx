953a0d6384a7883133c5351713c03c33d55eaa94
nahidchy <nicnahidchowdhury@gmail.com>
2024-05-22 10:20:54 +0600

    half done

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,6 +3,24 @@ const Blog = ({blog}) => {
     return (
         <div>
             <img src={blog.cover} alt="" />
+            <div className='flex justify-between'>
+                <div className='flex gap-5'>
+                <img className='w-12 border-yellow-900 border-2' src={blog.author_img} alt="" />
+                <div>
+                    <h3 className='text-2xl'>{blog.author}</h3>
+                    <p className='text-xl'>{blog.posted_date}</p>
+                </div>
+                </div>
+                <div>
+                  <span>{blog.reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{blog.title}</h2>
+            <p>
+                {
+                    blog.hashtags.map((hashtag,idx)=><span key={idx}><a href=''>{hashtag}</a></span>)
+                }
+            </p>
         </div>
     );
 };
